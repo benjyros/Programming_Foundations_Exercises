47863141dc89fcee272c4468aa87ae8f1f2ec47c
benjyros <benny.peterhans@gmail.com>
2025-09-22 12:21:11 +0200

    Solved: Chapter 02 - Exercise 2-12

--- Chapter 02/Exercise 2-12.py
+++ Chapter 02/Exercise 2-12.py
@@ -24,3 +24,24 @@
 # Amount the stock sold for: $85,500.00
 # Commission paid on the sale: $2,565.00
 # Profit (or loss if negative): $535.00
+
+COMMISSION_RATE = 0.03
+NUM_SHARES = 2000
+PURCHASE_PRICE = 40.0
+SELLING_PRICE = 42.75
+
+amount_paid = NUM_SHARES * PURCHASE_PRICE
+purchase_commission = amount_paid * COMMISSION_RATE
+total_paid = amount_paid + purchase_commission
+amount_sold = NUM_SHARES * SELLING_PRICE
+selling_commission = amount_sold * COMMISSION_RATE
+total_received = amount_sold - selling_commission
+profit_loss = total_received - total_paid
+
+print(f"""
+Amount paid for the stock: ${amount_paid:,.2f}
+Commission paid on the purchase: ${purchase_commission:,.2f}
+Amount the stock sold for: ${amount_sold:,.2f}
+Commission paid on the sale: ${selling_commission:,.2f}
+Profit (or loss if negative): ${profit_loss:,.2f}
+""")

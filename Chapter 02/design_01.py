a4f2913efada2a359d3e0f00139879bd003ab69c
benjyros <benny.peterhans@gmail.com>
2025-09-22 11:17:57 +0200

    Solved design_01

--- Chapter 02/design_01.py
+++ Chapter 02/design_01.py
@@ -28,3 +28,29 @@
 # Note: Use begin_fill() and end_fill() to fill the shapes with color
 #
 # Expected result: Two blue diamond shapes drawn on the screen
+
+import turtle
+
+turtle.hideturtle()
+turtle.fillcolor("blue")
+turtle.begin_fill()
+turtle.left(135)
+turtle.forward(100)
+turtle.left(90)
+turtle.forward(100)
+turtle.left(90)
+turtle.forward(100)
+turtle.left(90)
+turtle.forward(100)
+turtle.end_fill()
+
+turtle.begin_fill()
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(100)
+turtle.end_fill()
+turtle.done()
